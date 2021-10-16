144e07dc473d4b9c1f96872a95ac26052f9b1e1a
TanjaTomitsch <68329842+TanjaTomitsch@users.noreply.github.com>
2021-10-16 19:05:55 +0200

    Add files via upload

---
+++ scriptbahn.js
@@ -0,0 +1,23 @@
+function ButtonMore() {
+    console.log("more informations required")
+    alert("loading more informations")
+    window.location.href='https://wts2021.azurewebsites.net/capacity/';
+}
+
+function ButtonAlso() {
+    console.log("false")
+    document.getElementById("button").innerHTML="an error occured"; 
+    alert("this service is under construction")  
+    window.location.href='https://wts2021.azurewebsites.net/';   
+}
+
+function ButtonHome() {
+    console.log("Home")
+    alert("leading you home")
+    window.location.href='https://wts2021.azurewebsites.net/';   
+}
+
+
+
+
+
